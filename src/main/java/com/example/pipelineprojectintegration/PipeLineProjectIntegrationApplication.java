2d01bc9682b402fac0304a8736e74247fab11120
sushant.thapa55 <sushant.thapa55@gmail.com>
2022-06-27 20:49:46 +0545

    Initial commit

---
+++ src/main/java/com/example/pipelineprojectintegration/PipeLineProjectIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.pipelineprojectintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipeLineProjectIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipeLineProjectIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pipelineprojectintegration/PipeLineProjectIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pipelineprojectintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipeLineProjectIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
